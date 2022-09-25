8ca9b21528a5a2d517e8eab43774e44170aa5d3e
petkots <tsveta.v.petkova@gmail.com>
2022-09-25 23:20:07 +0200

    Controllers Created

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
 
+const homeController = require('./controllers/homeController');
+const defaultController = require('./controllers/defaultController');
+const catalogController = require('./controllers/catalogController');
+const createController = require('./controllers/createController');
+const defaultTitle = require('./middlewares/defaultTitle');
+
 const app = express();
 
 app.engine('.hbs', hbs.engine);
@@ -10,5 +16,13 @@ app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static('static'));
+
+app.use(defaultTitle('Accommodation Reservation'));
+
+app.use(homeController);
+app.use('/catalog', catalogController);
+app.use('/create', createController);
+
+app.all('*', defaultController)
 
 app.listen(3000, () => console.log('Server listening on port 3000'))
