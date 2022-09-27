10d0046c7e5d8d6acdae0d57c60368bc823a3740
petkots <tsveta.v.petkova@gmail.com>
2022-09-27 18:15:46 +0200

    acommodation

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,17 +1,18 @@
-const { getAll, getById } = require('../services/accomodationServices');
+const { getAll, getById } = require('../services/accommodationServices');
 
 const router = require('express').Router();
+
 
 router.get('/', (req, res) => {
     const search = req.query.search || '';
     const city = req.query.city || '';
     const fromPrice = Number(req.query.fromPrice) || 1;
     const toPrice = Number(req.query.toPrice) || 1000;
-
+    
     const rooms = getAll(search, city, fromPrice, toPrice);
 
     res.render('catalog', {
-        title: 'Accomodations',
+        title: 'All Accommodation',
         rooms,
         search,
         city,
